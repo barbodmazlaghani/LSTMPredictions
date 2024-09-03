f49bed58055a8e273e896c52711aa6f498830a75
barbodmazlaghani <b.amirmazlaghani@gmail.com>
2024-09-03 17:40:54 +0330

    rajab slope added

--- Tools/compute_slope_v2.py
+++ Tools/compute_slope_v2.py
@@ -1,35 +1,35 @@
 import pandas as pd
 
 # Define the file path for the Excel file
-file_path = 'ehsan/Car_Dena_93 h 955 - ir 44_Data_08_21_2024, 14_00_00_to_08_21_2024, 16_20_00_Siamak_Kan_SaeedAbad_Bargasht_Wedensday_alt_correct.xlsx'
-
+file_path = 'C:/Users/s_alizadehnia/Desktop/LSTMPredictions/Data/Rajab_641/generated/trip_8_3(date_8_13).csv'
+output_file_path = f'8_3_slope_added.csv'
 # Load the Excel file with all sheets
-excel_data = pd.ExcelFile(file_path)
+# excel_data = pd.ExcelFile(file_path)
 
-# Process each sheet in the Excel file
-for sheet_name in excel_data.sheet_names:
-    # Read each sheet into a DataFrame
-    data_df = pd.read_excel(file_path, sheet_name=sheet_name)
 
-    # Initialize the 'slope' column to 0.0
-    data_df['slope'] = 0.0
 
-    # Calculate slope for each block of 100 rows
-    for i in range(0, len(data_df) - 100, 100):
-        start_altitude = data_df.at[i, 'correct_altitude']
-        end_altitude = data_df.at[i + 100, 'correct_altitude']
-        start_mileage = data_df.at[i, 'Cumulative_mileage']
-        end_mileage = data_df.at[i + 100, 'Cumulative_mileage']
-        if end_mileage - start_mileage != 0:
-            slope = (end_altitude - start_altitude) / (end_mileage - start_mileage) / 10
-            data_df.loc[i:i + 99, 'slope'] = slope
 
-    # Define CSV file path for each sheet
-    output_file_path = f'{sheet_name}_slope_added.csv'
+data_df = pd.read_csv(file_path)
 
-    # Save the updated DataFrame to a CSV file
-    data_df.to_csv(output_file_path, index=False)
+# Initialize the 'slope' column to 0.0
+data_df['slope'] = 0.0
 
-    print(f"Updated file saved to {output_file_path}")
+# Calculate slope for each block of 100 rows
+for i in range(0, len(data_df) - 100, 100):
+    start_altitude = data_df.at[i, 'altitude']
+    end_altitude = data_df.at[i + 100, 'altitude']
+    start_mileage = data_df.at[i, 'Cumulative_mileage']
+    end_mileage = data_df.at[i + 100, 'Cumulative_mileage']
+    if end_mileage - start_mileage != 0:
+        slope = (end_altitude - start_altitude) / (end_mileage - start_mileage) / 10
+        data_df.loc[i:i + 99, 'slope'] = slope
+
+# Define CSV file path for each sheet
+
+
+# Save the updated DataFrame to a CSV file
+data_df.to_csv(output_file_path, index=False)
+
+print(f"Updated file saved to {output_file_path}")
 
 print("All sheets have been processed and saved as separate CSV files.")
